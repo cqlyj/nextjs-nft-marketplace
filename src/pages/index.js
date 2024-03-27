07de4534827642dcb563c28c15c35198e06ddae0
cqlyj <luoyingjie0721@gmail.com>
2024-03-27 21:54:18 +0800

    add home page

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta
+          name="description"
+          content="NFT Marketplace built with Next.js and Moralis"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>hello marketplace</div>
+    </>
+  );
+}
