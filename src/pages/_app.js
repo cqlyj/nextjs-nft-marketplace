80f1960bc640b0859a8facb12171a60f4ec400d6
cqlyj <luoyingjie0721@gmail.com>
2024-03-27 22:56:23 +0800

    add constants for contract address

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,23 @@
 import { MoralisProvider } from "react-moralis";
 import Header from "@/app/components/Header";
 import "../app/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <Header />
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta
+          name="description"
+          content="NFT Marketplace built with Next.js and Moralis"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <Header />
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta
-          name="description"
-          content="NFT Marketplace built with Next.js and Moralis"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div>hello marketplace</div>
     </>
   );
